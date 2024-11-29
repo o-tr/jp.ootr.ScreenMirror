4b6113ab80d8e9dfa86a20cd1ec331cae5cf5e0f
ootr <git@ootr.jp>
2024-11-29 13:54:40 +0900

    初期化

---
+++ Runtime/jp.ootr.ScreenMirror/Scripts/ScreenMirrorCapture.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace jp.ootr.ScreenMirror
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class ScreenMirrorCapture : UdonSharpBehaviour
+    {
+        [SerializeField] internal Camera targetCamera;
+        [SerializeField] internal int width = 1280;
+        [SerializeField] internal int height = 720;
+
+        private void OnEnable()
+        {
+            if (targetCamera == null) return;
+            targetCamera.targetTexture = new RenderTexture(width, height, 24);
+            targetCamera.enabled = true;
+        }
+    }
+}
---
+++ Runtime/jp.ootr.ScreenMirror/Scripts/ScreenMirrorScreen.cs
@@ -0,0 +1,29 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace jp.ootr.ScreenMirror
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class ScreenMirrorScreen : UdonSharpBehaviour
+    {
+        [SerializeField] internal ScreenMirrorCapture targetCapture;
+        [SerializeField] internal RawImage rawImage;
+        [SerializeField] internal AspectRatioFitter aspectRatioFitter;
+
+        public void OnEnable()
+        {
+            if (targetCapture == null) return;
+            if (aspectRatioFitter == null) return;
+            SendCustomEventDelayedFrames(nameof(Setup),1);
+        }
+        
+        public void Setup()
+        {
+            if (targetCapture == null) return;
+            if (aspectRatioFitter == null) return;
+            aspectRatioFitter.aspectRatio = (float)targetCapture.width / targetCapture.height;
+            rawImage.texture = targetCapture.targetCamera.targetTexture;
+        }
+    }
+}
