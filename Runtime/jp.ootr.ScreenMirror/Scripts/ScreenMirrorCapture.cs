f36bda756bdd222ab4e9557743c71bc9a4b8731b
ootr <git@ootr.jp>
2024-11-29 15:47:32 +0900

    親要素のスケールに対応

--- Runtime/jp.ootr.ScreenMirror/Scripts/ScreenMirrorCapture.cs
+++ Runtime/jp.ootr.ScreenMirror/Scripts/ScreenMirrorCapture.cs
@@ -9,12 +9,28 @@ namespace jp.ootr.ScreenMirror
         [SerializeField] internal Camera targetCamera;
         [SerializeField] internal int width = 1280;
         [SerializeField] internal int height = 720;
+        [SerializeField] internal CustomScaler.CustomScaler customScaler;
+        private float _defaultCameraSize;
 
         private void OnEnable()
         {
             if (targetCamera == null) return;
             targetCamera.targetTexture = new RenderTexture(width, height, 24);
             targetCamera.enabled = true;
+            _defaultCameraSize = targetCamera.orthographicSize;
+            UpdateCameraSize();
+            if (customScaler == null) return;
+            customScaler.AddEventListener(this);
+        }
+
+        public void OnScaleChanged()
+        {
+            UpdateCameraSize();
+        }
+        
+        private void UpdateCameraSize()
+        {
+            targetCamera.orthographicSize = _defaultCameraSize * targetCamera.transform.lossyScale.x;
         }
     }
 }
